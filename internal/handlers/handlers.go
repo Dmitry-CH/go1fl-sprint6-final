964308d45b50bb01b59dd1f724c27ffa47968b2c
Dmitriy Chekanov <dmit.checkanov@yandex.ru>
2025-04-26 20:31:03 +0300

    Add implementation of functions

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,63 @@
 package handlers
+
+import (
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func MainHandler(w http.ResponseWriter, req *http.Request) {
+	filePath := filepath.Join("../", "index.html")
+
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		http.Error(w, "ошибка сервера", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
+func UploadHandler(w http.ResponseWriter, req *http.Request) {
+	// резервируем место в оперативной памяти
+	req.ParseMultipartForm(10 << 20) // 10 MB maximum
+
+	// получаем файл из формы
+	file, fileHeader, err := req.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "ошибка при получении файла", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	// читаем файл и конвертируем
+	buff := make([]byte, fileHeader.Size)
+	file.Read(buff)
+
+	data, err := service.ConvertMorseOrText(string(buff))
+	if err != nil {
+		http.Error(w, "ошибка сервера", http.StatusInternalServerError)
+		return
+	}
+
+	// записываем в файл
+	fileName := time.Now().UTC().Format("2006-01-02")
+	fileExt := filepath.Ext(fileHeader.Filename)
+
+	filePath := filepath.Join("../", fileName+fileExt)
+
+	err = os.WriteFile(filePath, []byte(data), 0755)
+	if err != nil {
+		http.Error(w, "ошибка при записи файла", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(data))
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,20 @@
 package service
+
+import (
+	"regexp"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func ConvertMorseOrText(s string) (string, error) {
+	isText, err := regexp.MatchString(`[А-Яа-я]`, s)
+	if err != nil {
+		return "", err
+	}
+
+	if isText {
+		return morse.ToMorse(s), nil
+	}
+
+	return morse.ToText(s), nil
+}
